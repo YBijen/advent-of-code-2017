9e9876657d19d27cb03b82bfb7366398f98e6c89
Youri Bijen <YouriBijen@hotmail.com>
2021-01-06 20:41:55 +0100

    WIP Day 21 part 1 (Concept code)

--- src/Day21_cs/Program.cs
+++ src/Day21_cs/Program.cs
@@ -7,11 +7,65 @@ namespace Day21_cs
 {
     class Program
     {
+        private const string START_IMAGE = ".#...####";
         private static readonly Dictionary<string, string> _ruleBook = new Dictionary<string, string>();
 
         static void Main(string[] args)
         {
             CreateRuleBook(debug: true);
+            PerformRules();
+        }
+
+        private static void PerformRules(int iterations = 2)
+        {
+            var currentImage = START_IMAGE;
+            for (var i = 0; i < iterations; i++)
+            {
+                if(i == 0) // To start exception
+                {
+                    currentImage = _ruleBook[currentImage];
+                    continue;
+                }
+
+                var imageSize = (int)Math.Sqrt(currentImage.Length);
+
+                if (currentImage.Length % 2 == 0)
+                {
+                    var subImageList = new List<string>();
+
+                    var totalSquares = currentImage.Length / imageSize;
+                    var squaresPerLine = (int)Math.Sqrt(totalSquares);
+
+
+                    var currentSubImage = 0;
+                    for (var j = 0; j < totalSquares; j++)
+                    {
+
+                        var subImage = "";
+                        for(var x = 0; x < 2; x++)
+                        {
+                            var xModifier = j % squaresPerLine == 0 ? 0 : (j % squaresPerLine) * squaresPerLine; // How far is X away from the first char
+                            for (var y = 0; y < 2; y++)
+                            {
+                                var yModifier = (j / squaresPerLine) * squaresPerLine;
+                                Console.WriteLine($"Subimage {j}: {(x + xModifier)},{yModifier + y}");
+
+                                var idx = CalcIndexForRotation(currentImage.Length, imageSize, 0, (x + xModifier), (y + yModifier));
+                                subImage += currentImage[idx];
+                            }
+                        }
+                        Console.WriteLine();
+                        subImageList.Add(subImage);
+
+                    }
+
+                    var result = subImageList.Select(x => _ruleBook[x]).ToList();
+                }
+                else
+                {
+
+                }
+            }
         }
 
         private static void CreateRuleBook(bool debug)
